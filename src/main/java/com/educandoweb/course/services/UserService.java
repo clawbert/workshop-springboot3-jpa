7d50332c0ae4e34216a6db8e7d24bb73983687ac
clawbert <clawbertpereira@gmail.com>
2025-03-12 09:44:38 -0400

    User insert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -22,6 +22,10 @@ public class UserService {
         return user.get();
     }
 
+    public User insert(User user) {
+        return userRepository.save(user);
+    }
+
     @Autowired
     public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
