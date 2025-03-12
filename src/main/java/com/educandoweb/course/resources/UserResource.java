43431825e38e0bb4e4e3d25f8e8e6ba061ac0a0a
clawbert <clawbertpereira@gmail.com>
2025-03-12 10:26:34 -0400

    User update

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -42,6 +42,12 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        user = userService.update(id, user);
+        return ResponseEntity.ok().body(user);
+    }
+
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
